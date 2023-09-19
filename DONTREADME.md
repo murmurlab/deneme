a270df8cd24c3853cd986e48203dfca3dbca0412
Ahmet Faruk Basaran <ahbasara@k2m01s05.42kocaeli.com.tr>
2023-09-19 08:13:44 +0300

    a

---
+++ DONTREADME.md
@@ -0,0 +1,2 @@
+write my own library start from 42piscine subjects, then 42cursus
+a very good project, must be done!
---
+++ callocate.c
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+// ahbasara
+
+#include "murmurlibc.h"
+
+void
+	*callocate(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	memnull(ptr, count * size);
+	return (ptr);
+}
